543541386c21f390a2e6be1016cbc0dc713eaf1c
unknown <krisannmahinay@gmail.com>
2022-10-24 15:34:23 -0700

    fist commit

---
+++ admin/index.php
@@ -0,0 +1,39 @@
+<html>
+    <header>
+        <title>Food Order Website Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
+                </div>
+        </div>
+        <!-- Menu Section Ends-->
+
+        <!-- Main  Content Section Starts -->
+        <div class="main-content">
+        <div class="wrapper">
+                Main Content Goes Here
+                </div>
+        </div>
+         <!-- Main Content Section Ends-->
+
+        <!-- Footer Section Starts -->
+        <div class="footer">
+        <div class="wrapper">
+                <p class="text-center">2021 All Rights Reserved, Some Restaurant. Develop by- Kris Ann Mahinay <!-- link to my wbsite--></p>
+                </div>
+            </div>
+        <!-- Footer Section Ends-->
+        </body>
+    </html>
